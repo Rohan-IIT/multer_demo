26627804f70d61502e786be497b7e969576ce7bd
Rohan-IIT <rpawar3@hawk.iit.edu>
2023-11-13 11:51:28 -0600

    file uploading in upload folder

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('uploads'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+// Multer configuration for file upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  }
 });
+
+const upload = multer({ storage });
+
+// Routes
+app.get('/', (req, res) => {
+  const files = fs.readdirSync('uploads');
+  res.render('index');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  // File upload
+  console.log("file uploaded", req.body);
+  res.redirect('/');
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
